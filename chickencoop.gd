5ab1b578ca7ad8eee37d54a1aa627fc6fb810969
Lexizz7 <lexizz777@proton.me>
2025-04-27 17:38:16 -0300

    initial commit

---
+++ chickencoop.gd
@@ -0,0 +1,5 @@
+extends Area2D
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed:
+		get_tree().change_scene_to_file("res://scenes/chickencoop.tscn")
