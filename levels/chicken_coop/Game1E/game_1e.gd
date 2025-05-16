4d394ca92b3b658250267396f9bd9c74798ba64c
unknown <lexizz777@proton.me>
2025-05-16 19:01:27 -0300

    improve game

--- levels/chicken_coop/Game1E/game_1e.gd
+++ levels/chicken_coop/Game1E/game_1e.gd
@@ -3,38 +3,42 @@ extends Node2D
 const CARD_FRAME = preload("res://levels/chicken_coop/Game1E/card_frame/card_frame.tscn")
 
 @onready var card_spawn_point: Marker2D = $CardSpawnPoint
-@onready var total_score_label: Label = $TotalScore
-@onready var day_score_label: Label = $DayScore
-@onready var night_score_label: Label = $NightScore
+@onready var card_stack: Area2D = $CardStack
 
-var day_score: int = 0:
-	set(value):
-		day_score = value
-		day_score_label.text = str(value)
-		total_score_label.text = str(value + night_score)
+var day_score: int = 0
+var night_score: int = 0
 
-var night_score: int = 0:
-	set(value):
-		night_score = value
-		night_score_label.text = str(value)
-		total_score_label.text = str(value + day_score)
+var cards_dropped = 0
 
 func _ready():
 	spawn_card()
 
 func spawn_card():
+	if cards_dropped > 10:
+		return
+	elif  cards_dropped > 7:
+		card_stack.level = 3
+	elif cards_dropped > 4:
+		card_stack.level = 2
+	else:
+		card_stack.level = 1
+	
 	var card_instance = CARD_FRAME.instantiate()
 	card_instance.position = card_spawn_point.global_position
+	card_instance.visible = false
 	add_child(card_instance)
-
+	
+	card_instance.position = card_stack.global_position
+	card_instance.visible = true
 
 func _on_day_box_card_frame_dropped(isCorrect: bool) -> void:
 	if isCorrect:
 		day_score += 1
+	cards_dropped += 1
 	spawn_card()
-
 
 func _on_night_box_card_frame_dropped(isCorrect: bool) -> void:
 	if isCorrect:
 		night_score += 1
+	cards_dropped += 1
 	spawn_card()
--- utilities/scenes/player_transition_area.gd
+++ utilities/scenes/player_transition_area.gd
@@ -10,17 +10,22 @@ extends Area2D
 @onready var player := $"../Player"
 
 var isPlayerIn := false
+var isPlayerMoving := false
 
 func _ready() -> void:
 	if !player:
 		push_warning("PlayerTransitionArea must find a Player scene.")
 
 func _input_event(viewport, event, shape_idx):
+	if isPlayerMoving == true:
+		return
 	if event.is_action_pressed("tap") and player:
+		isPlayerMoving = true
 		await player.walk_to(global_position)
 		var tree = get_tree()
 		if tree and isPlayerIn and scenePath:
 			tree.change_scene_to_file(scenePath)
+	isPlayerMoving = false
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name != 'Player': return
