2aba23784604dbdd1da80bbc76ee1df08df71043
Arthur Ruiz <arthuranac2@gmail.com>
2025-05-24 17:38:30 -0300

    Game1A base finished

--- levels/celar/Game1A/animal/animal.gd
+++ levels/celar/Game1A/animal/animal.gd
@@ -1,4 +1,5 @@
 extends Node2D
+signal feed_droped
 
 var base = 0
 var to_eat = 0
@@ -17,6 +18,7 @@ func randomize():
 	to_eat = ((base - 1) / 3) + 1
 	print(to_eat)
 	label.text = str(to_eat)
+	feed_consumed = 0
 	
 func _update_animal_visibility():
 	for i in animal_sprites.size():
@@ -24,3 +26,12 @@ func _update_animal_visibility():
 		
 func get_to_eat() -> int:
 	return to_eat
+
+func is_fed():
+	return feed_consumed == to_eat
+
+func _on_dropzone_dropped(draggable: Area2D) -> void:
+	var feed_bag = draggable.get_parent()
+	feed_consumed += 1
+	emit_signal("feed_droped")
+	feed_bag.shrink_and_free()
--- levels/celar/Game1A/game_1a.gd
+++ levels/celar/Game1A/game_1a.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 const FEED_BAG = preload("res://levels/celar/Game1A/feed/feed.tscn")
 
+@onready var feed_left: Label = $feed_left
+@onready var score_label: Label = $Score
 @onready var feed_spawn: Marker2D = $feed_spawn
 @onready var animals: Array[Node2D] = [
 	$Animal,
@@ -10,20 +12,38 @@ const FEED_BAG = preload("res://levels/celar/Game1A/feed/feed.tscn")
 ]
 
 var total_feed = 0
+var score = 0
 
 func _ready() -> void:
+	score = 0
 	_setup()
 
 func _setup():
+	total_feed = 0
 	for animal in  animals:
 		animal.randomize()
 		total_feed += animal.get_to_eat()
 	_spawn_feed()
-		
+
 func _spawn_feed():
+	print(total_feed)
 	var feed_instance = FEED_BAG.instantiate()
 	feed_instance.position = feed_spawn.global_position
-	feed_instance.visible = true
 	add_child(feed_instance)
-	
-	
+	feed_left.text = str(total_feed)
+
+func _on_animal_feed_droped() -> void:
+	total_feed -= 1
+	if total_feed == 0:
+		_verify()
+		return
+	_spawn_feed()
+
+func _verify():
+	var is_correct: bool
+	for animal in  animals:
+		is_correct = animal.is_fed()
+	if is_correct:
+		score += 1
+		score_label.text = str(score)
+	_setup()
