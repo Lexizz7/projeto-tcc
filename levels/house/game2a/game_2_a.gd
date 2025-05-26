f3cab6aa71f8831375d4b5f983ba992bf82ee13c
Arthur Ruiz <arthuranac2@gmail.com>
2025-05-25 22:00:23 -0300

    partial game2a

--- levels/house/game2a/game_2_a.gd
+++ levels/house/game2a/game_2_a.gd
@@ -3,8 +3,16 @@ extends Node2D
 const APPLE = preload("res://levels/house/game2a/apple/apple.tscn")
 
 @onready var apple_spawn_points: Node = $AppleSpawnPoints
+@onready var thought_baloons := [
+	$thought_1/Label,
+	$thought_2/Label,
+	$thought_3/Label
+]
+
+var answer = 0
 
 func spawnApples(n: int):
+	answer = n
 	var markers = apple_spawn_points.get_children()
 	markers.shuffle()
 	markers = markers.slice(0, n)
@@ -13,6 +21,21 @@ func spawnApples(n: int):
 		var apple_instance = APPLE.instantiate()
 		apple_instance.position = marker.global_position
 		add_child(apple_instance)
+		
+func _randomize():
+	thought_baloons.shuffle()
+	var numbers = _number_generator(6)
+	spawnApples(numbers[0] + 5)
+	for i in range (0, 3):
+		thought_baloons[i].text = str(numbers[i] + 5)
+		
+func _number_generator(n: int):
+	var numbers = []
+	for i in range(1, n):
+		numbers.append(i)
+	numbers.shuffle()
+	return numbers.slice(0, 3)
+
 
 func _ready():
-	spawnApples(5)
+	_randomize()
