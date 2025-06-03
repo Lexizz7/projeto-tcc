bab98928e95a60102199a4368e5267210100f049
Pedro Henrique Silva Moreira <pedrohenriquesmoreira@gmail.com>
2025-06-03 02:43:47 -0300

    jogo 1 e 2 de Pedro att 2

--- levels/celar/Game3P/game_3p.gd
+++ levels/celar/Game3P/game_3p.gd
@@ -16,19 +16,12 @@ func _ready():
 func spawn_card():
 	if cards_dropped > 20:
 		return
-	elif  cards_dropped > 7:
-		card_stack.level = 3
-	elif cards_dropped > 4:
-		card_stack.level = 2
-	else:
-		card_stack.level = 1
 	
 	var card_instance = CARD_FRAME.instantiate()
 	card_instance.position = card_spawn_point.global_position
 	card_instance.visible = false
 	add_child(card_instance)
 	
-	card_instance.position = card_stack.global_position
 	card_instance.visible = true
 
 func _on_day_box_card_frame_dropped(isCorrect: bool) -> void:
