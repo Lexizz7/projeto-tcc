7ab59ff9fc8c3db056c9015ca978c119c2bdba8e
Lexizz7 <lexizz777@proton.me>
2025-06-30 20:41:25 -0300

    add pedro changes

--- levels/celar/Game1Pnew/game_1pnew.gd
+++ levels/celar/Game1Pnew/game_1pnew.gd
@@ -4,13 +4,18 @@ const CARD_FRAME = preload("res://levels/celar/Game1Pnew/card_frame/card_frame.t
 
 @onready var card_spawn_point: Marker2D = $CardSpawnPoint
 @onready var card_stack: Area2D = $CardStack
+@onready var results_tracker: ResultTracker = $ResultsTracker
 
-var day_score: int = 0
-var night_score: int = 0
+var a1: int = 0
+var a2: int = 0
+var a3: int = 0
+var a4: int = 0
+var a5: int = 0
 
 var cards_dropped = 0
 
 func _ready():
+	MetricsLogger.start_session("Game1P")
 	spawn_card()
 
 func spawn_card():
@@ -18,6 +23,7 @@ func spawn_card():
 		var tree = get_tree()
 		if tree:
 			tree.change_scene_to_file("res://levels/celar/barn.tscn")
+		MetricsLogger.end_session()
 		return
 	
 	var card_instance = CARD_FRAME.instantiate()
@@ -28,30 +34,61 @@ func spawn_card():
 	card_instance.visible = true
 
 func _on_animal1_card_frame_dropped(isCorrect: bool) -> void:
+	MetricsLogger.log_event("A1Dropped", {
+		"isCorrect": isCorrect
+	})
 	if isCorrect:
-		day_score += 1
+		results_tracker.add_hit()
+		a1 += 1
+	else:
+		results_tracker.add_miss()
 	cards_dropped += 1
 	spawn_card()
 
 func _on_animal2_card_frame_dropped(isCorrect: bool) -> void:
+	MetricsLogger.log_event("A2Dropped", {
+		"isCorrect": isCorrect
+	})
 	if isCorrect:
-		night_score += 1
+		results_tracker.add_hit()
+		a2 += 1
+	else:
+		results_tracker.add_miss()
 	cards_dropped += 1
 	spawn_card()
+		
 func _on_animal3_card_frame_dropped(isCorrect: bool) -> void:
+	MetricsLogger.log_event("A3Dropped", {
+		"isCorrect": isCorrect
+	})
 	if isCorrect:
-		day_score += 1
+		results_tracker.add_hit()
+		a3 += 1
+	else:
+		results_tracker.add_miss()
 	cards_dropped += 1
 	spawn_card()
 
 func _on_animal4_card_frame_dropped(isCorrect: bool) -> void:
+	MetricsLogger.log_event("A4Dropped", {
+		"isCorrect": isCorrect
+	})
 	if isCorrect:
-		night_score += 1
+		results_tracker.add_hit()
+		a4 += 1
+	else:
+		results_tracker.add_miss()
 	cards_dropped += 1
 	spawn_card()
 	
 func _on_animal5_card_frame_dropped(isCorrect: bool) -> void:
+	MetricsLogger.log_event("A5Dropped", {
+		"isCorrect": isCorrect
+	})
 	if isCorrect:
-		night_score += 1
+		results_tracker.add_hit()
+		a5 += 1
+	else:
+		results_tracker.add_miss()
 	cards_dropped += 1
 	spawn_card()
--- levels/celar/Game2Pnew/game_2pnew.gd
+++ levels/celar/Game2Pnew/game_2pnew.gd
@@ -4,20 +4,25 @@ const CARD_FRAME = preload("res://levels/celar/Game2Pnew/card_frame/card_frame.t
 
 @onready var card_spawn_point: Marker2D = $CardSpawnPoint
 @onready var card_stack: Area2D = $CardStack
+@onready var results_tracker: ResultTracker = $ResultsTracker
 
-var day_score: int = 0
-var night_score: int = 0
+var f1: int = 0
+var f2: int = 0
+var f3: int = 0
+var f4: int = 0
 
 var cards_dropped = 0
 
 func _ready():
+	MetricsLogger.start_session("Game2P")
 	spawn_card()
 
 func spawn_card():
-	if cards_dropped > 10:
+	if cards_dropped > 20:
 		var tree = get_tree()
 		if tree:
 			tree.change_scene_to_file("res://levels/celar/barn.tscn")
+		MetricsLogger.end_session()
 		return
 	
 	var card_instance = CARD_FRAME.instantiate()
@@ -28,25 +33,49 @@ func spawn_card():
 	card_instance.visible = true
 
 func _on_tool1_card_frame_dropped(isCorrect: bool) -> void:
+	MetricsLogger.log_event("F1Dropped", {
+		"isCorrect": isCorrect
+	})
 	if isCorrect:
-		day_score += 1
+		results_tracker.add_hit()
+		f1 += 1
+	else:
+		results_tracker.add_miss()
 	cards_dropped += 1
 	spawn_card()
 
 func _on_tool2_card_frame_dropped(isCorrect: bool) -> void:
+	MetricsLogger.log_event("F2Dropped", {
+		"isCorrect": isCorrect
+	})
 	if isCorrect:
-		night_score += 1
+		results_tracker.add_hit()
+		f2 += 1
+	else:
+		results_tracker.add_miss()
 	cards_dropped += 1
 	spawn_card()
 	
 func _on_tool3_card_frame_dropped(isCorrect: bool) -> void:
+	MetricsLogger.log_event("F3Dropped", {
+		"isCorrect": isCorrect
+	})
 	if isCorrect:
-		day_score += 1
+		results_tracker.add_hit()
+		f3 += 1
+	else:
+		results_tracker.add_miss()
 	cards_dropped += 1
 	spawn_card()
 	
 func _on_tool4_card_frame_dropped(isCorrect: bool) -> void:
+	MetricsLogger.log_event("F4Dropped", {
+		"isCorrect": isCorrect
+	})
 	if isCorrect:
-		day_score += 1
+		results_tracker.add_hit()
+		f4 += 1
+	else:
+		results_tracker.add_miss()
 	cards_dropped += 1
 	spawn_card()
